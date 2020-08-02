978c22dc5ac8f7f31a557af8c3ea906917f26bfc
Patrick Evans <ppevans11@gmail.com>
2020-08-02 17:46:15 +1200

    :sparkles: Added index.rb

---
+++ index.rb
@@ -0,0 +1,26 @@
+#!/usr/bin/env ruby
+
+# -------- #
+# index.rb #
+# -------- #
+
+# using SendGrid's Ruby Library
+# https://github.com/sendgrid/sendgrid-ruby
+
+require 'sendgrid-ruby'
+include SendGrid
+
+# ! Note: from address must be registered with sendgrid
+from = Email.new(email: 'test@example.com')
+to = Email.new(email: 'test@example.com')
+subject = 'Sending with SendGrid is Fun'
+content = Content.new(type: 'text/plain', value: 'and easy to do anywhere, even with Ruby')
+mail = Mail.new(from, subject, to, content)
+
+sg = SendGrid::API.new(api_key: ENV['SENDGRID_API_KEY'])
+response = sg.client.mail._('send').post(request_body: mail.to_json)
+puts response.status_code
+puts response.body
+puts response.headers
+
+# EOF #
